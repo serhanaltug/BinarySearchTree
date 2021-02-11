71e8f7b8126c51e83fb651906a99d8103d40f636
Serhan Altug <saltug@yahoo.com>
2021-02-11 16:05:03 +0300

    Add files via upload

---
+++ BinarySearchTree/EmptyBST.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class EmptyBST<T> : Tree<T> where T : IComparable
+    {
+        public EmptyBST()
+        {
+        }
+
+        public bool isEmpty()
+        {
+            return true;
+        }
+
+        public int cardinality()
+        {
+            return 0;
+        }
+
+        public bool member(T elt)
+        {
+            return false;
+        }
+
+        public NonEmptyBST<T> add(T elt) {
+            return new NonEmptyBST<T>(elt);
+        }
+    
+    }
+}
---
+++ BinarySearchTree/NonEmptyBST.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    public class NonEmptyBST<T> : Tree<T> where T : IComparable
+    {
+        T data;
+        Tree<T> left;
+        Tree<T> right;
+
+        public NonEmptyBST(T elt)
+        {
+            data = elt;
+            left = new EmptyBST<T>();
+            right = new EmptyBST<T>();
+        }
+
+        public NonEmptyBST(T elt, Tree<T> leftTree, Tree<T> rightTree) 
+        {
+            data = elt;
+            left = leftTree;
+            right = rightTree;
+        }
+
+        public bool isEmpty()
+        {
+            return false;
+        }
+
+        public int cardinality()
+        {
+            return 1 + left.cardinality() + right.cardinality();
+        }
+
+        public bool member(T elt)
+        {
+            if (elt.CompareTo(data) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                if (elt.CompareTo(data) < 0)
+                {
+                    return left.member(elt);
+                }
+                else
+                {
+                    return right.member(elt);
+                }
+            }
+        }
+
+        public NonEmptyBST<T> add(T elt)
+        {
+            if (elt.CompareTo(data) == 0)
+            {
+                return this;
+            }
+            else
+            {
+                if (elt.CompareTo(data) < 0) {
+                    return new NonEmptyBST<T>(data, left.add(elt), right);
+                } else {
+                    return new NonEmptyBST<T>(data, left, right.add(elt));                
+                }
+            }
+        }
+
+    }
+}
